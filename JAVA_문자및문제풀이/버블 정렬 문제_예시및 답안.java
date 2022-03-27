9e546ba6a735161461fee229697e6c05a852c428
LeeSeungSub <zlzm2522@gmail.com>
2022-03-27 12:11:36 +0900

    feat: 버블정렬_식

--- JAVA_문자및문제풀이/버블 정렬 문제_예시및 답안.java
+++ JAVA_문자및문제풀이/버블 정렬 문제_예시및 답안.java
@@ -9,7 +9,7 @@ int array22[] = {14,11}; //{0,1} <==버블정렬 알고리즘.
 			int temp = 0;
 			for(int i=1; i<size;++i) {
 				for(int j=0; j<size-1; ++j) {
-					if(array[i] < array[i]) {
+					if(array[i] < array[j]) {
 						temp = array[i];//array[i]를 임시공간에 잠시 넣어둔다.
 						array[i] = array[j];
 						array[j] = temp;
