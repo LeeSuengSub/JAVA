0ff18e27fbed66e4d2159bac77adccd6cd51119b
LeeSuengSub <zlzm2522@gmail.com>
2022-04-26 15:00:14 +0900

    feat: 글씨 깨짐_수정

--- static설명/Chef.java
+++ static설명/Chef.java
@@ -1,10 +1,10 @@
-package ???;
+package 스태틱;
 
 public class Chef {
 
-	private static Chef chef = null;//????? ??? ??? ??
+	private static Chef chef = null;//스태틱으로 선언된 클래스 변수
 	
-	public static Chef getInstance() { //????? ??? ???
+	public static Chef getInstance() { //스태택으로 선언된 메소드
 		if(chef ==null) {
 			chef = new Chef();
 		}
