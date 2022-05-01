07c27361009a3f0dfec9d35ca41d4c8b8c412b52
LeeSuengSub <zlzm2522@gmail.com>
2022-05-01 18:57:12 +0900

    feat: 오버라이딩과 다형성

---
+++ inflearn/Do_it 자바프로그램임_입문/.java/오버라이딩과 다형성(4)(4-3).java
@@ -0,0 +1,37 @@
+package whithArrayList;
+
+public class VIPCustomer extends Customer{
+
+	private int agentID;
+	double saleRatio;
+	
+	public VIPCustomer() {
+		super();//자기의 바로 상위 클래스
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		saleRatio = 0.1;
+		
+//		System.out.println("VIPCostomer() 호출");
+	}
+	public VIPCustomer(int customerID, String customerName, int agentID) {
+		super(customerID, customerName);
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		saleRatio = 0.1;
+		this.agentID = agentID;
+	}
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price - (int)(price * saleRatio);
+	}
+	public int getAgentID() {
+		return agentID;
+	}
+	@Override
+	public String showCustomerInfo() {
+		//super를 써야 상위 클래스를 부른다.
+		return super.showCustomerInfo()+"담당 상담원아이디는 "+agentID+" 입니다.";
+	}
+	
+	
+}
